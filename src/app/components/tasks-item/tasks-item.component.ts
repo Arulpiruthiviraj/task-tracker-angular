d70f0645ddb2a96b42b29ed933bb51b25a48e11a
arularunthavaraja <arul.arunthavaraja@davinci-retail.com>
2021-10-01 14:50:40 -0400

    chnages

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css'],
+})
+export class TasksItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onTaskDelete: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+
+  faTimes = faTimes;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  handleTaskDelete(task: Task) {
+    this.onTaskDelete.emit(task);
+  }
+  handleOnToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+}
